a32f091292f9c6dc27e64aa8d60fb93fe2629bdd
Alexandre Sauner <alexandre@trackit.io>
2021-02-07 14:35:58 +0100

    add registration of users

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
+const {
+  WAITING_FOR_EMAIL, WAITING_FOR_PASSWORD, WAITING_FOR_2FA, REGISTERED,
+} = require('./constants');
+
+const QAcache = [];
+const users = [];
 
 console.log('starting bot.');
 
@@ -8,5 +14,53 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN || '', {
 });
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Hello there!');
+  bot.sendMessage(msg.chat.id, 'Please type in your Epitech mail.');
+  QAcache.push({
+    id: msg.chat.id,
+    state: WAITING_FOR_EMAIL,
+  });
+});
+
+bot.on('message', (msg) => {
+  if (msg.text == null || msg.text.startsWith('/')) return;
+
+  const QAindex = QAcache.findIndex((x) => x.id === msg.chat.id);
+  if (QAindex == null) {
+    bot.sendMessage(msg.chat.id, 'Please use /start to begin.');
+    return;
+  }
+
+  switch (QAcache[QAindex].state) {
+    case WAITING_FOR_EMAIL:
+      bot.sendMessage(msg.chat.id, 'Please type in your password.');
+      QAcache[QAindex] = {
+        ...QAcache[QAindex],
+        state: WAITING_FOR_PASSWORD,
+        email: msg.text,
+      };
+      break;
+
+    case WAITING_FOR_PASSWORD:
+      bot.sendMessage(msg.chat.id, 'Please type in your 2FA token.');
+      QAcache[QAindex] = {
+        ...QAcache[QAindex],
+        state: WAITING_FOR_2FA,
+        password: msg.text,
+      };
+      break;
+
+    case WAITING_FOR_2FA:
+      bot.sendMessage(msg.chat.id, 'You are now registered.');
+      users.push({
+        ...QAcache[QAindex],
+        '2fa': msg.text,
+        state: REGISTERED,
+      });
+      QAcache.splice(QAindex, 1);
+      break;
+
+    default:
+      bot.sendMessage(msg.chat.id, 'Unknown state. Please use /start to begin.');
+      break;
+  }
 });
